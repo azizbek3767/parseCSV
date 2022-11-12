7f6d7101234a3242d0b405765058488ffa2c1fad
azizbekk3767 <azizbekk3767@gmail.com>
2022-11-13 02:29:00 +0500

    initial

---
+++ ParseCSVFile/ChessPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ParseCSVFile
+{
+    public class ChessPlayer
+    {
+        public int Rank, Raiting, Games, B_Year;
+        public char Title;
+        public string Name, Country;
+
+        public ChessPlayer(string rawData)
+        {
+            var data = rawData.Split(';');
+            this.Rank = Convert.ToInt32(data[0]);
+            this.Name = data[1];
+            this.Title = Convert.ToChar(data[2]);
+            this.Country = data[3];
+            this.Raiting = Convert.ToInt32(data[4]);
+            this.Games = Convert.ToInt32(data[5]);
+            this.B_Year = Convert.ToInt32(data[6]);
+        }
+
+        public override string ToString()
+        {
+            string outcome = $"Full Name: {Name}: " +
+                $"Country: {Country}" +
+                $"Title: {Title}, Rank: {Rank}, Raiting: {Raiting}, Games: {Games}, B-Year: {B_Year}";
+            return outcome;
+
+        }
+
+    }
+}
---
+++ ParseCSVFile/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+
+namespace ParseCSVFile
+{
+    internal class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        static void Main(string[] args)
+        {
+            var rawCsvLines = System.IO.File.ReadAllLines(@"C:\Test assesment\Top100ChessPlayers.csv");
+            var chessPlayers = new List<ChessPlayer>();
+
+            for (int i = 1; i < rawCsvLines.Length; i++)
+            {
+                var chessPlayer = new ChessPlayer(rawCsvLines[i]);
+                chessPlayers.Add(chessPlayer);
+            }
+
+            var olderChessPlayers = chessPlayers.FindAll(cp => cp.B_Year > 1980);
+
+            for (int i = 0; i < 10; i++)
+            {
+                log.Info(olderChessPlayers[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
